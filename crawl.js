30cc8ba21d0f563d4c7d0d342816aae4bddde43d
Ranjan <ranzan.in@zohomail.in>
2026-04-17 12:02:17 +0000

    normalizeURL with some successfull tests

--- crawl.js
+++ crawl.js
@@ -1,9 +1,18 @@
 function  normalizeUrl(urlString) {
     const urlObj = new URL(urlString);
-    const hostPath = `${urlObj.hostname}${urlObj.pathname}`;
+    const hostPath = `${urlObj.hostname}${urlObj.pathname.toLocaleLowerCase()}`;
+
+    /*
+         urlObj.pathname was giving actual pathname if CAPITAL it giving capital which causes test failure.
+
+         So, to make it work used toLocaleLowerCase() to convert it to small letters.
+    */
+
+    // for trailing slash
     if (hostPath.length > 0 && hostPath.slice(-1) === "/") {
         return hostPath.slice(0, -1);
     }
+
     return hostPath;
 }
 
--- crawl.test.js
+++ crawl.test.js
@@ -8,3 +8,24 @@ test("normalizeURL",  () => {
     const expected = "rnzn.onslate.in/contact";
     expect(actual).toEqual(expected);
 })
+
+test("normalizeURL/with trailing slash",  () => {
+    const input = "https://rnzn.onslate.in/contact/";
+    const actual = normalizeUrl(input);
+    const expected = "rnzn.onslate.in/contact";
+    expect(actual).toEqual(expected);
+})
+
+test("normalizeURL/with capital letters",  () => {
+    const input = "https://RNZN.onslate.in/contact/";
+    const actual = normalizeUrl(input);
+    const expected = "rnzn.onslate.in/contact";
+    expect(actual).toEqual(expected);
+})
+
+test("normalizeURL/with all capital letters",  () => {
+    const input = "https://RNZN.ONSLATE.IN/CONTACT/";
+    const actual = normalizeUrl(input);
+    const expected = "rnzn.onslate.in/contact";
+    expect(actual).toEqual(expected);
+})
